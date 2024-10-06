594106598234f376289dd8d2ee2054b5c0ed3624
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-06 06:45:33 +0000

    Oct 6, 2024, 10:45 AM

---
+++ vitaltrack/vt/static/js/index.js
@@ -0,0 +1,9 @@
+const burger_button = document.getElementById("burger-menu");
+const span1 = document.getElementById("span1");
+const span2 = document.getElementById("span2");
+const span3 = document.getElementById("span3");
+const navbar = document.getElementById("navbar");
+
+burger_button.addEventListener("click", function() {
+
+})
