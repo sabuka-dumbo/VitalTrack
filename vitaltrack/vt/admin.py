0ec0b32f3889b3a6ac65e2b0dc5d57841591cfa4
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-06 12:13:23 +0000

    Oct 6, 2024, 4:13 PM

--- vitaltrack/vt/admin.py
+++ vitaltrack/vt/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.register(User)
-admin.register(WaterIntake)
-admin.register(Food)
-admin.register(FoodIntake)
-admin.register(Mood)
+admin.site.register(User)
+admin.site.register(WaterIntake)
+admin.site.register(Food)
+admin.site.register(FoodIntake)
+admin.site.register(Mood)
--- vitaltrack/vt/views.py
+++ vitaltrack/vt/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,13 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm = request.POST.get('confirm')
+        print(username, email, password, confirm)
+
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'register.html')
