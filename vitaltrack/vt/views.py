e1bc1e747fee7a9c28d1690474e8e93650492561
Sabuka-Dumbo <anonym.sas@proton.me>
2024-10-06 12:21:18 +0000

    Oct 6, 2024, 4:21 PM

--- vitaltrack/vt/views.py
+++ vitaltrack/vt/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from .models import *
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -15,7 +17,20 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm = request.POST.get('confirm')
-        print(username, email, password, confirm)
+
+        if password != confirm:
+            print("Passwords does not match:(")
+            return HttpResponseRedirect(reverse('register'))
+        
+        if "@" not in email:
+            print("Please enter correct email:(")
+            return HttpResponseRedirect(reverse('register'))
+        
+        if User.objects.all().filter(email=email):
+            print("Email is already taken:(")
+            return HttpResponseRedirect(reverse('register')) 
+        
+        
 
         return HttpResponseRedirect(reverse('index'))
     else:
